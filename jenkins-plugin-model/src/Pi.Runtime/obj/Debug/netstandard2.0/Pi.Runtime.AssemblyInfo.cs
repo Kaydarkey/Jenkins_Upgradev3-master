bc5948f7a01e1a15a266f27ecc2d3917d6ba837c
Kaydarkey <darkeykafui@gmail.com>
2025-02-17 11:05:26 +0000

    commit update

--- jenkins-plugin-model/src/Pi.Runtime/obj/Debug/netstandard2.0/Pi.Runtime.AssemblyInfo.cs
+++ jenkins-plugin-model/src/Pi.Runtime/obj/Debug/netstandard2.0/Pi.Runtime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi.Runtime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e0093eb82112e8837c8ce73572714f157903ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi.Runtime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi.Runtime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
